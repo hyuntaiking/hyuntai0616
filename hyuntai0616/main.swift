ed34270ea5853b6ef5237487a29ae69651d1799a
hyuntaiking <hyuntaiking@gmail.com>
2017-06-16 08:54:54 +0800

    Initial Commit

---
+++ hyuntai0616/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  hyuntai0616
+//
+//  Created by Hyuntai on 2017/6/16.
+//  Copyright © 2017年 hyphen. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
